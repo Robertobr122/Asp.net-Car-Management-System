151c3b8c2fb3abc7bc00359f6387b4d19d5b06a2
robertobr122 <rbbragaengenharia@gmail.com>
2024-12-27 10:49:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using minimal_api.domain.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Olá Pessoal");
+
+app.MapPost("/login", (minimal_api.domain.DTOs.LoginDto loginDto) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+app.Run();
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
+
